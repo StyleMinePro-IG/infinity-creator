6dfac808bfaf3d41ac6d9007753027f351da91b8
StyleMinePro-IG <aatishpc1@outlook.com>
2025-03-02 19:34:22 +0530

    Create maths.cpp

---
+++ maths.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using name space;
+int main()
+{
+  int a, b, ,c d, e, f, g, h, i, j, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+float sum, sub, multi, divi, remi, sq, rc, cir, area, cir, area1, circm1;
+cout<<"hello user!"<<"\n can you enter a choise you need from bellow"<<endl;
+cout<<"[1] Addition \n" <<"[2] Subraction"<<"[3] Multiplication"<<"[3] Division"<<"[4] Reminder"<<"[5] More option";
+return 0;
+}
+  
