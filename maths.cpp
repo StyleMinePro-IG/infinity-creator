cb7c19b66f5c9c12c296b3009f9556a7418aeb1b
StyleMinePro-IG <aatishpc1@outlook.com>
2025-03-02 19:51:52 +0530

    Update maths.cpp

--- maths.cpp
+++ maths.cpp
@@ -6,6 +6,26 @@ int main()
 float sum, sub, multi, divi, remi, sq, rc, cir, area, cir, area1, circm1;
 cout<<"hello user!"<<"\n can you enter a choise you need from bellow"<<endl;
 cout<<"[1] Addition \n" <<"[2] Subraction"<<"[3] Multiplication"<<"[3] Division"<<"[4] Reminder"<<"[5] More option";
+  cout<<"enter your choice:";
+  cin<<a;
+  switch(a)
+    {
+      case 1 : cout<<"enter two no.";
+                     cin>>b>>c; sum=b+c;
+                     cout<<"your sum is:"<<sum;
+                     break;
+      case 2 : cout<<"enter two no.";
+                     cin>>b>>c;
+                     sub=b-c;
+                     cout<<"your difference is:"<< sub;
+                     break;
+      case 3 : cout<<"enter two no.";
+                      cin>>b>>c;
+                      multi=b*c;
+                      cout<<"the product is:"<<multi;
+                      break;
+        }
 return 0;
 }
   
+                        
